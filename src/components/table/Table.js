65b959c8d151122f6e80505eb04d480635d12ee7
Aleksandr Mihasenko <aleksandr.mihasenko@gmail.com>
2022-02-13 13:48:49 +0300

    add changing width and height logic

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -21,25 +21,38 @@ export class Table extends ExcelComponent {
       const $parent = $resizer.closest('[data-type="resizable"]');
       const coords = $parent.getCoords();
       const type = $resizer.data.resize;
-      const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`);
+      let value;
+
+      $resizer.css({ opacity: 1 });
 
       document.onmousemove = (e) => {
         if (type === 'col') {
           const delta = e.pageX - coords.right;
-          const value = coords.width + delta;
+          value = coords.width + delta;
 
-          $parent.css({ width: value + 'px' });
-          cells.forEach((el) => el.style.width = value + 'px');
+          $resizer.css({ right: -delta + 'px' });
         } else {
           const delta = e.pageY - coords.bottom;
-          const value = coords.height + delta;
+          value = coords.height + delta;
 
-          $parent.css({ height: value + 'px' });
+          $resizer.css({ bottom: -delta + 'px' });
         }
       };
 
       document.onmouseup = () => {
         document.onmousemove = null;
+        document.onmouseup = null;
+
+        if (type === 'col') {
+          $parent.css({width: value + 'px'});
+          this.$root
+              .findAll(`[data-col="${$parent.data.col}"]`)
+              .forEach((el) => el.style.width = value + 'px');
+          $resizer.css({ opacity: 0, right: '-2px' });
+        } else {
+          $parent.css({ height: value + 'px' });
+          $resizer.css({ opacity: 0, bottom: '-2px'});
+        }
       };
     }
   }
