cbe1ab1c3cd8149a1046c674d4791fed049a427c
Aleksandr Mihasenko <aleksandr.mihasenko@gmail.com>
2022-03-02 09:15:21 +0300

    add functionality allocating choosing columns

---
+++ src/components/table/TableSelection.js
@@ -0,0 +1,13 @@
+export class TableSelection {
+  constructor() {
+    this.group = [];
+  }
+
+  // $el instanceof Dom === true
+  select($el) {
+    this.group.push($el);
+    $el.addClass('__selected');
+  }
+
+  selectGroup() {}
+}
--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,13 +3,16 @@ const CODES = {
   Z: 90,
 };
 
-function toCell(_, col) {
-  return `
-    <div
-      class="excel__table__row__data__cell"
-      contenteditable data-col="${col}"
-     ></div>
-  `;
+function toCell(row) {
+  return function(_, col) {
+    return `
+      <div
+        class="excel__table__row__data__cell"
+        contenteditable data-col="${col}"
+        data-id="${row}:${col}"
+      ></div>
+    `;
+  };
 }
 
 function toColumn(col, index) {
@@ -56,12 +59,12 @@ export function createTable(rowsCount = 15) {
 
   rows.push(createRow(null, cols));
 
-  for (let i = 0; i < rowsCount; i++) {
+  for (let row = 0; row < rowsCount; row++) {
     const cells = new Array(colsCount)
         .fill('')
-        .map(toCell)
+        .map(toCell(row))
         .join('');
-    rows.push(createRow(i + 1, cells));
+    rows.push(createRow(row + 1, cells));
   }
 
   return rows.join('');
