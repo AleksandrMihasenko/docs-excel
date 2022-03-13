dda28e53055b997079e2bcde5d0f5ebf004564bf
Aleksandr Mihasenko <aleksandr.mihasenko@gmail.com>
2022-03-13 14:21:19 +0300

    minor fixes

--- src/core/Dom.js
+++ src/core/Dom.js
@@ -98,10 +98,12 @@ class Dom {
 
   addClass(className) {
     this.$el.classList.add(className);
+    return this;
   }
 
   removeClass(className) {
     this.$el.classList.remove(className);
+    return this;
   }
 }
 
