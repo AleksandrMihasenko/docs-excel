d97871e05eb68258fb5952452595536b087c61ea
Aleksandr Mihasenko <aleksandr.mihasenko@gmail.com>
2022-05-06 11:30:29 +0300

    refactoring

--- src/components/toolbar/toolbar.template.js
+++ src/components/toolbar/toolbar.template.js
@@ -22,17 +22,17 @@ export function createToolbar(state) {
   const buttons = [
     {
       icon: 'format_align_left',
-      isActive: false,
+      isActive: state['textAlign'] === 'left',
       value: { textAlign: 'left' },
     },
     {
       icon: 'format_align_center',
-      isActive: false,
+      isActive: state['textAlign'] === 'center',
       value: { textAlign: 'center' },
     },
     {
       icon: 'format_align_right',
-      isActive: false,
+      isActive: state['textAlign'] === 'right',
       value: { textAlign: 'right' },
     },
     {
@@ -42,13 +42,17 @@ export function createToolbar(state) {
     },
     {
       icon: 'format_italic',
-      isActive: false,
-      value: { fontStyle: 'italic' },
+      isActive: state['fontStyle'] === 'italic',
+      value: { fontStyle: state['fontStyle'] === 'italic'
+        ? 'normal'
+        : 'italic' },
     },
     {
       icon: 'format_underline',
-      isActive: false,
-      value: { textDecoration: 'underline' },
+      isActive: state['textDecoration'] === 'underline',
+      value: { textDecoration: state['textDecoration'] === 'underline'
+        ? 'none'
+        : 'underline' },
     },
   ];
   return buttons.map(toButton).join('');
